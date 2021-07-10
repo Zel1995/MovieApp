f9c7e28ae55ddc72a1a92515ae2399fad216114e
Zel1995 <glocen@mail.ru>
2021-07-10 19:13:15 +0300

    add firebase notification and token

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -6,21 +6,27 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.movieapp.R.string.hello
 import com.example.movieapp.di.App
 import com.example.movieapp.domain.AirplaneModeReceiver
 import com.example.movieapp.domain.router.MainRouter
+import com.example.movieapp.fcm.FcmRepository
 import com.example.movieapp.ui.MainActivityModule
 import com.example.movieapp.ui.MainSubcomponent
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.main_activity),
     BottomNavigationView.OnNavigationItemSelectedListener {
     @Inject
     lateinit var router: MainRouter
+
+    @Inject
+    lateinit var fcmRepository: FcmRepository
     private lateinit var receiver: AirplaneModeReceiver
     var mainSubcomponent: MainSubcomponent? = null
+    val mainScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +46,13 @@ class MainActivity : AppCompatActivity(R.layout.main_activity),
                 router.openMovieListFragment()
             }
             R.id.favorite -> {
-                Toast.makeText(applicationContext, resources.getString(hello), Toast.LENGTH_SHORT)
-                    .show()
+                mainScope.launch {
+                    fcmRepository.getToken().let {
+                        Toast.makeText(applicationContext, it, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
             }
             R.id.history -> {
                 router.openHistoryFragment()
